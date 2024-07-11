ccc3de2e70bd5700ef35384a08d8e8b6c3218523
Mishal-A <mishal45654@gmail.com>
2024-07-11 11:31:43 +0500

    second commit for themes

--- Expense Tracker/tracker/src/App.js
+++ Expense Tracker/tracker/src/App.js
@@ -4,6 +4,7 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import ThemeSwitcher from './components/ThemeSwitcher.js';
 
 import { GlobalProvider } from './context/GlobalState.js';
 
@@ -12,6 +13,7 @@ import './App.css';
 function App() {
   return (
     <GlobalProvider>
+      <ThemeSwitcher/>
       <Header />
       <div className="container">
         <Balance />
